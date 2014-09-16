e25f94c45b56248df4ac197d7b8711ced7f80337
mac <mac@macdeiMac.local>
2014-09-16 13:57:38 +0800

    Initial Commit

---
+++ ChinaMobileCommunity/CMCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CMCAppDelegate.h
+//  ChinaMobileCommunity
+//
+//  Created by mac on 14-9-16.
+//  Copyright (c) 2014年 zhangyanqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChinaMobileCommunityTests/ChinaMobileCommunityTests.m
@@ -0,0 +1,34 @@
+//
+//  ChinaMobileCommunityTests.m
+//  ChinaMobileCommunityTests
+//
+//  Created by mac on 14-9-16.
+//  Copyright (c) 2014年 zhangyanqiu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChinaMobileCommunityTests : XCTestCase
+
+@end
+
+@implementation ChinaMobileCommunityTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
